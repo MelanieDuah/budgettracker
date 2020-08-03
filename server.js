225e6c0e4141727dd4d4c752e4c9921b59cb8806
melanie <melanieduah@gmail.com>
2020-08-03 01:37:12 -0500

    Cleaned up package.json file to exclude unnecessary packages

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
 
 const PORT = 9000;
 
 const app = express();
 
-app.use(logger("dev"));
-
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/budgettracker", {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/budgettracker", 
+{
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
